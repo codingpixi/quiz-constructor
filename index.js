bba2078bfd6c24e54be852474a0906dc0a4aead2
ddsheard <ddsheard@gmail.com>
2017-04-03 18:50:01 -0400

    question now shows

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function Quiz (quizQuestion, correctAnswer, possibleAnswers) {
   this.possible = possibleAnswers;
   }
 
-let question1 = new Quiz ('The type that specifies what kind of event occured is', 'event type', ['event type', 'even target', 'both a and b', 'none of the mentioned'] )
+let question1 = new Quiz ('The type that specifies what kind of event occured is', 'Event Type', ['Event Type', 'Even Target', 'Both A and B', 'None of the Mentioned'] )
 // console.log(question1);
 
 let questionArray = [question1];
-console.log(question1);
+console.log(questionArray);
 
 
 let source = document.querySelector('#quiz-template').innerHTML;
 let template = Handlebars.compile(source);
-let html =questionArray.map(object => template(object));
+let html = questionArray.map(object => template(object));
 let destination = document.querySelector('.handlebars-demo');
-destination.innerHTML = html
+destination.innerHTML = html;
