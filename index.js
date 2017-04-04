1537907d85b61d72a6379ca6102971bcb989a062
ddsheard <ddsheard@gmail.com>
2017-04-04 13:24:53 -0400

    last info from last night

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
+let source = document.querySelector('#quiz-template').innerHTML;
+let template = Handlebars.compile(source);
+
 
 function Quiz (quizQuestion, correctAnswer, possibleAnswers) {
   this.question = quizQuestion;
   this.correct = correctAnswer;
   this.possible = possibleAnswers;
-  }
+  this.display = function(){
 
-let question1 = new Quiz ('The type that specifies what kind of event occured is', 'Event Type', ['Event Type', 'Even Target', 'Both A and B', 'None of the Mentioned'] )
+  let questionArray = [question1];
+
+  // console.log(questionArray);
+
+  let html = questionArray.map(object => template(object));
+  let destination = document.querySelector('.handlebars-demo');
+  destination.innerHTML = html;
+
+  let correctAnswer = document.querySelector("span.answer");
+  let possibleAnswers = document.querySelectorAll('li');
+  for (i=0; i<possibleAnswers.length; i++){
+    let possibleAnswer i
+  }
+  rightAnswer.addEventListener('click', pickles)
+  function pickles(event) {
+    console.log(event);
+  }
+  }
+    }
+
+let question1 = new Quiz ('In general, event handler is nothing but', 'Function', ['Interface', 'Event', 'Handler'] )
 // console.log(question1);
 
-let questionArray = [question1];
-console.log(questionArray);
 
 
-let source = document.querySelector('#quiz-template').innerHTML;
-let template = Handlebars.compile(source);
-let html = questionArray.map(object => template(object));
-let destination = document.querySelector('.handlebars-demo');
-destination.innerHTML = html;
+
+
+
+question1.display();
