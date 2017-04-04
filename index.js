bdd41afdfe28268495a934b633d4f5b8ece7e132
ddsheard <ddsheard@gmail.com>
2017-04-04 16:57:11 -0400

    got the first item to click

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-let source = document.querySelector('#quiz-template').innerHTML;
+let source = document.querySelector('#quiz').innerHTML;
 let template = Handlebars.compile(source);
 
 
-function Quiz (quizQuestion, correctAnswer, possibleAnswers) {
-  this.question = quizQuestion;
-  this.correct = correctAnswer;
-  this.possible = possibleAnswers;
-  this.display = function(){
+function Quiz(quizQuestion, correctAnswer, possibleAnswers) {
+    this.question = quizQuestion;
+    this.correct = correctAnswer;
+    this.possible = possibleAnswers;
+    this.display = function() {
 
-  let questionArray = [question1];
+        //maybe get rid of this line
+        let questionArray = [question1];
 
-  // console.log(questionArray);
+        //let html = template(this): FROM CLASS
 
-  let html = questionArray.map(object => template(object));
-  let destination = document.querySelector('.handlebars-demo');
-  destination.innerHTML = html;
+        // console.log(questionArray);
 
-  let correctAnswer = document.querySelector("span.answer");
-  let possibleAnswers = document.querySelectorAll('li');
-  for (i=0; i<possibleAnswers.length; i++){
-    let possibleAnswer i
-  }
-  rightAnswer.addEventListener('click', pickles)
-  function pickles(event) {
-    console.log(event);
-  }
-  }
+        let html = questionArray.map(object => template(object));
+        let destination = document.querySelector('.handlebars-demo');
+        destination.innerHTML = html;
+
+        let correctAnswer = document.querySelector("span.answer");
+        let clickedAnswer = document.querySelector('.correct');
+        clickedAnswer.addEventListener('click', theCorrectAnswer);
+
+        function theCorrectAnswer(event) {
+          correctAnswer.innerText = "yippeeee";
+        }
+
+
+        // let possibleAnswers = document.querySelectorAll('li');
+        // for (i = 0; i < possibleAnswers.length; i++) {
+        //     let possibleAnswers = [i];
+        // }
+        //   let
+        //   rightAnswer.addEventListener('click', pickles)
+        //
+        // function pickles(event) {
+        //     console.log(event);
+        // }
     }
+}
 
-let question1 = new Quiz ('In general, event handler is nothing but', 'Function', ['Interface', 'Event', 'Handler'] )
+let question1 = new Quiz('In general, event handler is nothing but', 'Function', ['Interface', 'Event', 'Handler'])
 // console.log(question1);
 
 
