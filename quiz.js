5df330ae2d3616fdaba732941d4b9a4cab1009bc
ddsheard <ddsheard@gmail.com>
2017-04-05 09:09:53 -0400

    update questions

--- quiz.js
+++ quiz.js
@@ -24,19 +24,24 @@ function Question(text, answer) {
 function MultipleChoiceQuestion (text, answer, choices){
   Question.call(this, text, answer);
   this.choices = choices;
+  console.log(answer);
 }
-Question.prototype = Object.create(MultipleChoiceQuestion.prototype);
+
+MultipleChoiceQuestion.prototype = Object.create(Question.prototype);
 
 MultipleChoiceQuestion.prototype.choices = function () {
   this.question = this.question + this.answer + this.choices;
 }
-let question
-
+let q1 = new MultipleChoiceQuestion ('q1', 'a1', ['a1', 'a2', 'a3', 'a4']);
+let q2 = new MultipleChoiceQuestion ('q2', 'a1', ['a1', 'a2', 'a3', 'a4']);
+let q3 = new MultipleChoiceQuestion ('q3', 'a1', ['a1', 'a2', 'a3', 'a4']);
+let q4 = new MultipleChoiceQuestion ('q4', 'a1', ['a1', 'a2', 'a3', 'a4']);
+let q5 = new MultipleChoiceQuestion ('q5', 'a1', ['a1', 'a2', 'a3', 'a4']);
 // function ShortAnswerQuestion (openAnswer) {
 //   this.openAnswer = openAnswer;
 // }
 
 
-let q1 = new Question("q1", ['a1', 'a2', 'a3', 'a4']);
+// let q1 = new Question("q1", ['a1', 'a2', 'a3', 'a4']);
 let q2 = new Question("q2", ['b1', 'b2', 'b3', 'b4'], 'an array');
 [q1, q2].forEach(question => question.display());
